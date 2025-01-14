070d7764320a174464b7166eafc1cd39dfda02dd
Max6453 <146641020+Max6453@users.noreply.github.com>
2025-01-14 14:59:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,52 +7,50 @@ const app = express();
 app.use(express.static(__dirname))
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect('mongodb://localhost:27017/PureRxcing')
+mongoose.connect('mongodb://localhost:27017/Pure-Rxcing')
 const db = mongoose.connection
 db.once('open', ()=>{
     console.log('Successfully connected to MongoDB')
 })
 
 const userSchema = new mongoose.Schema({
-    regd_no: String,
+    about: String,
+    first_name: String,
+    last_name: String,
     email: String,
-    About: String,
-    First_Name: String,
-    Last_Name: String,
-    City: String,
-    State: String,
-    Postal_code: String,
+    city: String,
+    region: String,
+    postal_code: String,
 })
 
-const Users = mongoose.model("data",userSchema)
+const Users = mongoose.model("Contact-Form",userSchema)
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname,'/contact.html'))
 })
 
 app.post('/post',async (req,res) => {
-    const {regd_no,
-        About,
-        First_Name,
-        Last_Name,
+    const {
+        about,
+        first_name,
+        last_name,
         email,
-        City,
-        State,
-        Postal_code} = req.body
+        city,
+        region,
+        postal_code} = req.body
 
     const user = new Users({
-        regd_no,
+        about,
+        first_name,
+        last_name,
         email,
-        About,
-        First_Name,
-        Last_Name,
-        City,
-        State,
-        Postal_code,
+        city,
+        region,
+        postal_code,
     })
     await user.save()
     console.log(user)
-    res.send("form submitted")
+    res.send("form submitted, Please return to the previous page")
 })
 
 app.listen(port, () => {
